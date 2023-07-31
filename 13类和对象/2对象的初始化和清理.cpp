6ada580400e0e1c80b435e4e974d73f184e47fae
zywkuo <zywkuo@foxmail.com>
2023-07-31 14:57:25 +0800

    对象初始化和清理

--- 13类和对象/2对象的初始化和清理.cpp
+++ 13类和对象/2对象的初始化和清理.cpp
@@ -85,6 +85,81 @@ void test03()
   Person p = doWork2();
 };
 
+class Person1
+{
+
+public:
+  // 初始哈列表初始化属性
+  Person1(int a, int b, int c) : m_A(a), m_B(b), m_C(c){};
+
+  int m_A;
+  int m_B;
+  int m_C;
+};
+
+class Phone
+{
+public:
+  Phone(string pName)
+  {
+    m_PName = pName;
+  }
+
+  string m_PName;
+};
+class Person2
+{
+public:
+  Person2(string name, string pName) : m_Name(name), m_Phone(pName){};
+
+  string m_Name;
+  Phone m_Phone;
+};
+
+void test04()
+{
+  Person2 p("张三", "苹果Max");
+  cout << p.m_Name << "拿着" << p.m_Phone.m_PName << endl;
+};
+
+class Person3
+{
+public:
+  static int m_A;
+  int m_B;
+
+  static void func()
+  {
+    m_A = 200;
+    // m_B = 200; 错误，静态函数不可访问非静态成员变量
+    cout << "static void func 调用" << endl;
+  };
+
+private:
+  static int m_C; // 类外无法访问
+  static void func2(){
+      // 类外无法访问
+  };
+};
+
+int Person3::m_A = 100;
+
+void test05()
+{
+  Person3 p;
+  cout << p.m_A << endl;
+
+  Person3 p2;
+  p2.m_A = 200;
+  cout << p.m_A << endl;
+
+  //  通过类名访问
+  cout << Person3::m_A << endl;
+
+  p.func();
+  Person3::func();
+};
+
 // 构造函数的分类及调用
 int main()
 {
@@ -99,7 +174,32 @@ int main()
   // 拷贝构造函数使用时机
 
   // test02();
+
   test03();
 
+  // 深拷贝与浅拷贝
+  // m_Height = new int(*p.m_Height);
+
+  // 初始化列表
+  Person1 p(3, 2, 1);
+  cout << p.m_A << p.m_B << p.m_C << endl;
+
+  // 类对象作为类成员
+  test04();
+
+  // 静态成员
+  // static
+  /**
+   * 静态成员变量
+   * 所有对象共享同一份数据
+   * 在编译阶段分配内存
+   * 类内声明，类外初始化
+   */
+  /**
+   * 静态成员函数
+   * 所有对象共享同一函数
+   * 静态成员函数只能访问静态成员变量
+   */
+  test05();
   return 0;
 }
